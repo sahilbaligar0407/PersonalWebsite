31ec3dc1f4e5d810486fcc8faeda9ef42f60cd76
Sahil Baligar <sbaligar@purdue.edu>
2026-02-24 18:26:54 -0500

    Added direct link to vote.

--- app/Senate/page.tsx
+++ app/Senate/page.tsx
@@ -22,6 +22,14 @@ export default function SenatePage() {
           <p className="text-base text-gray-300">
             Scan the QR code to cast your vote.
           </p>
+          <p className="mt-2">
+            <a
+              href="https://purdue.ca1.qualtrics.com/jfe/form/SV_cZvk5GOr3l99JNs"
+              className="inline-block px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              Click Here to Vote
+            </a>
+          </p>
         </header>
 
         <section className="w-full">
